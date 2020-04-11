1aa8496b174deedb2521244b2b23f27d4cee75f1
JackFlynt <50310027+JackFlynt@users.noreply.github.com>
2020-04-11 15:31:42 +0930

    Added initial version of bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('OwO'):
+        await message.channel.send('Ew, furry')
+
+client.run(TOKEN)
