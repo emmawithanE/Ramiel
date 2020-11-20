e62638cae6c085e2edcbe91d5633fed437d49c61
JackFlynt <a1687142@student.adelaide.edu.au>
2020-11-20 23:59:52 +1030

    Added alternative trigger for test response
    Added discord.ext reference to test for interference

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,7 +18,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('OwO'):
+    if message.content.startswith('OwO') or message.content.startswith('uwu'):
         await message.channel.send('Ew, furry')
 
 client.run(TOKEN)
