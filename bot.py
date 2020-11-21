d8151ae639e30df5a92ef1d803ef66465f1f3e11
JackFlynt <a1687142@student.adelaide.edu.au>
2020-11-21 23:36:16 +1030

    Successful implementation of initial discord.ext pattern commands, will start on the actual getting of names and all that tomorrow

--- bot.py
+++ bot.py
@@ -7,18 +7,33 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.default()
 
-@client.event
+bot = commands.Bot(
+	command_prefix="R: ",
+	owner_id=268721746708791297,
+	intents = intents
+	)
+
+@bot.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+    print('Logged in as {0.user}, now printing more'.format(bot))
 
-@client.event
+# testing responding to keywords
+
+@bot.command()
+async def ping(ctx):
+	await ctx.send('pong!')
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('OwO') or message.content.startswith('uwu'):
         await message.channel.send('Ew, furry')
 
-client.run(TOKEN)
+    await bot.process_commands(message)
+
+
+bot.run(TOKEN)
