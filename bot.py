1f113cef1f622bcb8764663f6999fb2fc93e220a
JackFlynt <a1687142@student.adelaide.edu.au>
2020-11-22 00:38:22 +1030

    Added logging of names in local names.txt, contents survive through bot restarting

--- bot.py
+++ bot.py
@@ -15,6 +15,11 @@ bot = commands.Bot(
 	intents = intents
 	)
 
+# creation and loading of storage file
+namefile = open("names.txt", "r+")
+names = namefile.readlines()
+namefile.close()
+
 @bot.event
 async def on_ready():
     print('Logged in as {0.user}, now printing more'.format(bot))
@@ -25,12 +30,30 @@ async def on_ready():
 async def ping(ctx):
 	await ctx.send('pong!')
 
+@bot.command()
+async def add(ctx, arg):
+	names.append(arg)
+	await ctx.send('Added %s to names!' %arg)
+
+@bot.command()
+async def list(ctx):
+	for name in names:
+		await ctx.send(name)
+
+@bot.command()
+async def save(ctx):
+	namefile = open("names.txt","w+")
+	for name in names:
+		namefile.write(name + "\n")
+	namefile.close()
+	await ctx.send("Saved!")
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith('OwO') or message.content.startswith('uwu'):
+    if message.content.startswith('OwO') or message.content.startswith('uwu') or message.content.startswith('owo'):
         await message.channel.send('Ew, furry')
 
     await bot.process_commands(message)
